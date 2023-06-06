be01f590278d51dbd67cd8e5c573f1fa2f32487e
Valentin <valentinpicard.tech@gmail.com>
2023-06-06 10:08:57 +0200

    Chapitre 3 - Partie E : Dimensionnement

--- Chapitre3/DiscoverGUI/src/Fenetre.java
+++ Chapitre3/DiscoverGUI/src/Fenetre.java
@@ -29,6 +29,26 @@ public class Fenetre extends JFrame {
         cp.add(label, BorderLayout.NORTH);
         cp.add(b1, BorderLayout.SOUTH);
 
+        JPanel a = new JPanel();
+        JPanel b = new JPanel();
+        JPanel c = new JPanel();
+
+        a.setBackground( Color.RED );
+        b.setBackground( Color.GREEN  );
+        c.setBackground( Color.BLUE );
+
+        a.setMaximumSize( new Dimension(  10, 10) );
+        b.setMaximumSize( new Dimension(  50, 10) );
+
+        JPanel panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+
+        panel.add(a);
+        panel.add(b);
+        panel.add(c);
+
+        cp.add(panel);
+
         setVisible(true);
     }
 
