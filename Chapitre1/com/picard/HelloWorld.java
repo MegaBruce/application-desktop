127761c4ef0337cb9845b704d784104f6188e813
Valentin <valentinpicard.tech@gmail.com>
2023-06-06 20:32:31 +0200

    Chapitre 1 - Partie A : Utilisation de VS code

---
+++ Chapitre1/com/picard/HelloWorld.java
@@ -0,0 +1,9 @@
+package com.picard;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        for (int index = 0; index < 5; index++) {
+            System.out.println("Je veux essayer VS code");
+        }
+    }
+}
