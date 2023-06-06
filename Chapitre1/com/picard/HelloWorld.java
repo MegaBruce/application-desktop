8a6cb3128d3117f855f7fd7ae011aa61df4996cb
ynov-valentin <135751588+ynov-valentin@users.noreply.github.com>
2023-06-06 20:49:41 +0200

    Update HelloWorld.java

--- Chapitre1/com/picard/HelloWorld.java
+++ Chapitre1/com/picard/HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         for (int index = 0; index < 5; index++) {
             System.out.println("Je veux essayer VS code");
+            joj
         }
     }
 }
