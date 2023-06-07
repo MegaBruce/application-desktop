db563ce35c00f86f11f07b1d5d28eaf209d10ed1
Valentin <valentinpicard.tech@gmail.com>
2023-06-07 17:10:22 +0200

    Chapitre 1, partie B : Introduction IntelliJ

--- Chapitre1/discoverIntelliJ/src/main/java/HelloWorld.java
+++ Chapitre1/discoverIntelliJ/src/main/java/HelloWorld.java
@@ -1,14 +1,11 @@
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class MaPremiereClass {
-    private static Logger logger = LoggerFactory.getLogger(MaPremiereClass.class);
+public class HelloWorld {
+    private static Logger logger = LoggerFactory.getLogger(HelloWorld.class);
     public static void main(String[] args) {
         for (int i = 0; i < 5; i++) {
             logger.info("Hello World");
         }
-    }
-    public int add(int a, int b) {
-        return a+b;
     }
 }
